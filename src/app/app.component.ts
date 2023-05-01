1b9f7b57c7cf742eb7c5c5c480f883208a629c0c
unknown <getshay02@gmail.com>
2023-05-01 10:16:13 -0500

    Deployment test 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tf_arquiweb';
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/inicio').subscribe(data => {
+      console.log(data);
+    });
+  }
 }
