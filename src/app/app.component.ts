434907760cec87d0dbcc14e355f021a8af96e4fd
unknown <getshay02@gmail.com>
2023-05-01 10:46:18 -0500

    cambios deployment 10:46 1-05-2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.http.get('http://localhost:3000/inicio').subscribe(data => {
+    this.http.get('http://localhost:5000/inicio').subscribe(data => {
       console.log(data);
     });
   }
